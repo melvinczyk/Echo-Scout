d4d8fe8c20ca71164af91c360fe5dd1aac06c9d1
Nick Burczyk <78186753+melvinczyk@users.noreply.github.com>
2026-05-06 14:57:53 -0500

    added more touch

--- Echo_Scout/include/imu_screen.h
+++ Echo_Scout/include/imu_screen.h
@@ -2,15 +2,15 @@
 #define IMU_SCREEN_H
 
 namespace ImuScreen {
-    constexpr int CUBE_SIZE = 55;
-    constexpr int CUBE_CX = 120;
-    constexpr int CUBE_CY = 148;
-    constexpr int CUBE_YMAX = 235;
+    constexpr int CUBE_SIZE  = 55;
+    constexpr int CUBE_CX    = 120;
+    constexpr int CUBE_CY    = 145;
+    constexpr int CUBE_YMAX  = 230;
     constexpr float PROJ_DIST = 280.0f;
 }
 
-void tickIMU();
-void drawImuScreen();
 void drawImuBase();
+void tickIMU();
+void handleImuTouch(int tx, int ty);
 
 #endif
